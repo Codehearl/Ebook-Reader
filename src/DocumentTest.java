d819dd3af67ff2d96240832c22f1b51a0863ef74
Codehearl <sanusiadeyemi66@gmail.com>
2023-06-18 23:49:49 +0100

    Added some Unit tests

--- src/DocumentTest.java
+++ src/DocumentTest.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class DocumentTest {
     //TODO write tests for the methods and classes
     @org.junit.jupiter.api.Test
-    void parse() {
+    void testTokenizeEqual() {
+        /* TODO */
+    }
+    void testTokenizerError(){
+        //Todo
+    }
+
+    void testNormalizerPass(){
+        //TODO
+    }
+    void testNormalizerFail(){
+        //TODO
+    }
+
+    void testStemWordsEquals(){
+        //TODO
     }
 }
